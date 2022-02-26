f7530e1c549a679028b27d3f0c96f2078d42740e
Edgar Muralles <edmuralles@gmail.com>
2022-02-26 11:47:03 -0600

    Cambios sabado 26 febrero 2022 12pm

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const form = document.getElementsByTagName("form")[0];
+/** @type {HTMLInputElement} */
+const inputCodigo = document.getElementById("codigo");
+/** @type {HTMLInputElement} */
+const inputNombre = document.getElementById("nombre");
+/** @type {HTMLInputElement} */
+const inputCantidad = document.getElementById("cantidad");
+/** @type {HTMLInputElement} */
+const inputPrecio = document.getElementById("precio");
+/** @type {HTMLInputElement} */
+const selectCategoria = document.getElementById("categoria");
 const tbody = document.getElementsByTagName("tbody")[0];
 const cantidadTotalElement = document.getElementById("cantidad-total");
 const precioTotalElement = document.getElementById("precio-total");
@@ -8,6 +18,7 @@ let indice = 0;
 let cantidadTotal = 0;
 let preciosTotales = 0;
 let granTotal = 0;
+let currentRow;
 
 form.addEventListener("submit", onSubmit);
 
@@ -22,8 +33,9 @@ function onSubmit(event)
     const data = new FormData(form);
     const values = Array.from(data.entries());
 
-    [frmNombre, frmCantidad, frmPrecio, frmCategoria] = values;
+    [frmCodigo, frmNombre, frmCantidad, frmPrecio, frmCategoria] = values;
 
+    let codigo = frmCodigo[1];
     const nombre = frmNombre[1];
     const cantidad = frmCantidad[1];
     const precio = frmPrecio[1];
@@ -34,10 +46,22 @@ function onSubmit(event)
     preciosTotales = parseFloat(precio) + preciosTotales;
     granTotal = parseFloat(total) + granTotal;
 
-    indice++;
-    const tr = document.createElement("tr");
+    if (!codigo)
+    {
+        indice++;
+        codigo = indice;
+        tr = document.createElement("tr");
+        tbody.appendChild(tr);
+    }
+    else
+    {
+        tr = currentRow;
+    }
+
+    tr.dataset.categoria = categoria;
+    //console.log(tr.dataset.categoria);
     tr.innerHTML = `
-        <td>${indice}</td>
+        <td>${codigo}</td>
         <td>${nombre}</td>
         <td>${cantidad}</td>
         <td>${precio}</td>
@@ -45,8 +69,6 @@ function onSubmit(event)
         <td><a href="#" onclick="onEdit(event)">Editar</a> | <a href="#" onclick="onDelete(event)">Eliminar</a></td>
     `;
     
-    tbody.appendChild(tr);
-
     cantidadTotalElement.innerText = cantidadTotal;
     precioTotalElement.innerText = preciosTotales;
     granTotalElement.innerText = granTotal;
@@ -66,6 +88,15 @@ function onEdit(event)
     const anchor = event.target;
     const tr = anchor.parentElement.parentElement;
     const celdas = tr.getElementsByTagName("td");
+    const [tdCodigo, tdNombre, tdCantidad, tdPrecio] = celdas;
+    
+    inputCodigo.value = tdCodigo.innerText;
+    inputNombre.value = tdNombre.innerText;
+    inputCantidad.value = tdCantidad.innerText;
+    inputPrecio.value = tdPrecio.innerText;
+    selectCategoria.value = tr.dataset.categoria;
+
+    currentRow = tr;
     console.log(celdas);
 }
 
